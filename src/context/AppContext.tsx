5b9324c420592ba442adf187a9c2f6552a94a8d0
soumya-99 <soumyadeepmondal1234@gmail.com>
2025-02-14 15:06:43 +0530

    Version upgraded to 1.5 to 2.0

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -9,7 +9,7 @@ export const AppStore = createContext<any>(null)
 const AppContext = ({ children }) => {
     const appState = useRef(AppState.currentState)
     // debugging
-    const uat = true
+    const uat = false
 
     const [isLogin, setIsLogin] = useState<boolean>(() => false)
     const [isLoading, setIsLoading] = useState<boolean>(() => false)
