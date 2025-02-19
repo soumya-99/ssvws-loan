043055e357b5af87ccedb00415ed52025355f289
Utsab Roy <roy.utsab01@gmail.com>
2025-02-19 18:23:29 +0530

    App 190225

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://192.168.1.239:3014"
-export const BASE_URL = "http://202.21.38.178:3014"
+// export const BASE_URL = "http://202.21.38.178:3014"
 // export const BASE_URL = "http://192.168.1.9:3014"
 
-// export const BASE_URL = "https://ssvwsadmin.opentech4u.co.in"
+export const BASE_URL = "https://ssvwsadmin.opentech4u.co.in"
